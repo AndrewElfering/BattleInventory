819b19124509e943e2bace27be945eea73f69909
Andrew Elfering <93015578+AndrewElfering@users.noreply.github.com>
2025-06-02 13:22:27 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,9 @@ public class Main {
 
         Inventory inventory = new Inventory(); 
 
-        Item item1 = new Item("Generic Item", 10);
         Ration ration = new Ration("MRE", 10, "food");
         Weapon weapon = new Weapon("Axe", 4, 75, "bladed");
 
-        inventory.addItem(item1);
         inventory.addItem(ration);
         inventory.addItem(weapon);
         inventory.displayInventory();
